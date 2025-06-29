a62ba35b4118e544e617a9a313d32fb40c04e7db
No-Alex <nokhrin.av@mail.ru>
2025-06-29 15:03:33 +0500

    1. list_reverse.py - завершено описание функции в модуле (вся обработка - внутри функции) 2. list_reverse_def.py - альтернативный вариант, определение границ диапазона выполнено в модуле, функция выполняет только формирование и вывод диапазона от max к min

--- list_reverse.py
+++ list_reverse.py
@@ -1,4 +1,4 @@
-def create_range_decr_2_int(num_max, num_min):
+def create_range_decr_2_int(max_min):
     """На вход программа получает через запятую два целых числа start и stop. При этом start может быть меньше stop, а может быть больше.
     Создайте диапазон чисел с шагом 1 в обратном порядке от наибольшего числа к наименьшему (включительно). Выведите результат через запятую.
 
@@ -11,12 +11,12 @@ def create_range_decr_2_int(num_max, num_min):
     Sample Output 2:
     22, 21, 20, 19, 18, 17, 16, 15
     """
-    startstop = "22, 9"
-    start, stop = list(map(int, startstop.split(", ")))
-    if start > stop:
-        print(list(range(start, stop - 1, -1)))
+    num_max, num_min = list(map(int, max_min.split(", ")))
+    if num_max > num_min:
+        print(list(range(num_max, num_min - 1, -1)))
     else:
-        print(list(range(stop, start - 1, -1)))
+        print(list(range(num_min, num_max - 1, -1)))
 
-
-create_range_decr_2_int(23, 9)
+# call function
+startstop = "25, 9"
+create_range_decr_2_int(startstop)

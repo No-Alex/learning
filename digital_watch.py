27b63c7dafc38f360884296f800172040efc66b3
No-Alex <nokhrin.av@mail.ru>
2025-07-08 21:58:46 +0500

    Внесены финальные изменения по замечаниям в PR #13

--- digital_watch.py
+++ digital_watch.py
@@ -1,30 +1,30 @@
 # преобразовать произвольное число минут (возможно, более 1 суток) в показания цифровых часов
-# TODO - переименовать по-питоновски модуль и идентификаторы
-# [x] выполнено
-# В Python преимущественно используется нотация under_score
+"""
+переименовать по-питоновски модуль и идентификаторы
+- [x] выполнено
+в Python преимущественно используется нотация under_score
+
+"""
+"""
+создать дополнительные строковые объекты с применением:
+  - [x] f-строк
+  - [x] функции format
+  - [x] %
+
+"""
 
 total_minutes = int(12305)
 
 watch_minutes = total_minutes % 60
 full_days = total_minutes // (60*24)
 watch_hours = (total_minutes // 60) - (full_days*24)
-# добавить добивку нулями слева
-# s_watch_hours = "0" + str(watch_hours)
-# s_watch_hours = s_watch_hours[-2:]
-# замена на .rjust()
+
+# добавлена добивка нулями слева
 s_watch_hours = str(watch_hours).rjust(2, "0")
-# s_watch_minutes = "0" + str(watch_minutes)
-# s_watch_minutes = s_watch_minutes[-2:]
-# замена на .rjust()
 s_watch_minutes = str(watch_minutes).rjust(2, "0")
 actual = s_watch_hours + ':' + s_watch_minutes
 print(actual)
-"""
-строка 14 - создать дополнительные строковые объекты с применением:
-  - [x] f-строк
-  - [x] функции format
-  - [x] %
-"""
+
 # f-string
 actual_2 = f"{s_watch_hours}:{s_watch_minutes}"
 print(actual_2)

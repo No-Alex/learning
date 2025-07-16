1941352889347291ee9a833aaa6788c225d2b72e
No-Alex <nokhrin.av@mail.ru>
2025-07-16 21:20:27 +0500

    Создан модуль, описана функция

---
+++ test_code/last_digit_of_number.py
@@ -0,0 +1,20 @@
+"""
+Дано натуральное число
+Выведите его последнюю цифру
+"""
+
+
+def get_last_digit(num: int) -> int:
+    """возвращает последнюю цифру числа
+
+    Args:
+        num:
+
+    Returns:
+
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    assert get_last_digit(123) == 3
